0cff9aa908755a0e7660237fabea209b44219c34
Scott <scotthenry1@gmail.com>
2023-03-10 18:37:19 -0500

    test deploy

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function About() {
   return (
-       <div>About Us!</div>
+        <div className="justify-center container mx-auto bg-gray-200 rounded-xl border p-8 m-10 h-screen">
+          <h1 className="text-2xl">Hello!</h1>
+          <br></br>
+          <p className="font-bold text-xl max-w-75%">Welcome to our library's webpage!</p>
+          <br></br>
+          <p>Our goal is to provide a welcoming and inclusive space where everyone can access resources and information to support their personal and professional growth.At our library, you'll find a wide range of books and genres to check out for free. Please help us grow our database by filling out our New Book Form and spreading the word about our website.</p>
+    </div>  
   );
 }
 
